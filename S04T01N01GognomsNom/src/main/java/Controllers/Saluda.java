c0eba75f5353be4fde7293d25a947c5ffa566c86
Pepe <107991714+jramma@users.noreply.github.com>
2022-09-27 10:23:01 +0200

    Add files via upload

---
+++ S04T01N01GognomsNom/src/main/java/Controllers/Saluda.java
@@ -0,0 +1,21 @@
+package Controllers;
+
+
+public class Saluda {
+	private final long id;
+	private final String texto;
+	
+	
+	public Saluda(long id, String texto) {
+		this.id = id;
+		this.texto = texto;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getNombre() {
+		return texto;
+	}
+	
+	
+}
