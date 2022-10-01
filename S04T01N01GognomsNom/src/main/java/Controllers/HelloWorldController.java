478b130ed6dbfd6cf4c3d937eddd52e0fdf47acc
Pepe <107991714+jramma@users.noreply.github.com>
2022-10-01 20:16:06 +0200

    Add files via upload

--- S04T01N01GognomsNom/src/main/java/Controllers/HelloWorldController.java
+++ S04T01N01GognomsNom/src/main/java/Controllers/HelloWorldController.java
@@ -1,7 +1,9 @@
-package Controllers;
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.S04T01N01GognomsNom.controllers;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +19,13 @@ public class HelloWorldController {
 	public Saluda saluda(@RequestParam(value = "nombre", defaultValue = "UNKNOWN") String nombre) {
 		return new Saluda(counter.incrementAndGet(), String.format(template, nombre));
 	}
-	@GetMapping("/HelloWorld2")
-	public Saluda saluda2(@PathVariable(value = "nombre") String nombre) {
+	
+	
+// https://www.baeldung.com/spring-optional-path-variables	
+	@GetMapping(value={"/HelloWorld2","/HelloWorld2/{nombre}"})
+	public Saluda saluda2(@PathVariable Optional <String> nombre, Model model) {
+		if(nombre.isEmpty())
+			nombre=Optional.of("UNKNOW");
 		return new Saluda(counter.incrementAndGet(), String.format(template, nombre));
 	}
 
